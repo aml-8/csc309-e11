c855cc92f973139c28752a8484fb5d349458caae
Adreano La Rosa <adreano.larosa@mail.utoronto.ca>
2025-04-01 03:33:46 -0400

    stuff

---
+++ my-app/app/csc309-e11/page.tsx
@@ -0,0 +1,11 @@
+const obj = () => {
+
+    return(
+        <div id="auto-tester">
+  Hi. I am __utorid__, and I have learned how to develop a full-stack web app
+  and deploy it!
+</div>
+    );
+}
+
+export default obj;
